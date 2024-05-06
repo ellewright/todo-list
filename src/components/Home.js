b61771187b696a3dc89d16c5e93928045c1c0db8
Eleanor Wright <elliewright055@gmail.com>
2024-05-06 18:52:22 -0500

    Additional structuring

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,15 @@ export default function Home() {
         <div className="header">
           <h1>To-Do List</h1>
         </div>
-        <div className="input-field">
-          <input type="text" placeholder="Purchase groceries..."></input>
+        <div className="new-input">
+          <div className="input-field">
+            <input type="text" placeholder="Purchase groceries..."></input>
+          </div>
+          <div className="submit-button">
+            <button type="submit">Submit</button>
+          </div>
         </div>
-        <div className="submit-button">
-          <button type="submit">Submit</button>
-        </div>
+        <div className="list"></div>
       </div>
     )
   }
